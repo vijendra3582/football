1eee66e31f036888e01f6ee22602db9ff895a68e
Vijendra <vijendra3582@gmail.com>
2020-09-07 14:57:23 +0530

    home

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -18,62 +18,68 @@ import { LoginGuard } from './../guards/login.guard';
 export const adminRoutes: Routes = [
     {
         path: '',
-        canActivate: [LoginGuard],
+        canActivate: [],
         component: LoginComponent
     },
     {
-        path: "login",
-        canActivate: [LoginGuard],
-        component: LoginComponent
-    },
-    {
-        path: "forget",
-        canActivate: [LoginGuard],
-        component: ForgetComponent
-    },
-    {
-        path: "dashboard",
-        canActivate: [AuthGuard],
-        component: DashboardComponent
-    },
-    {
-        path: "orgnization",
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
+        path: 'v',
+        canActivate: [],
         children: [
-            { path: "create", component: OrgnizationCreateComponent },
-            { path: "edit/:id", component: OrgnizationCreateComponent },
-            { path: "manage", component: OrgnizationListComponent },
+            {
+                path: "login",
+                canActivate: [LoginGuard],
+                component: LoginComponent
+            },
+            {
+                path: "forget",
+                canActivate: [LoginGuard],
+                component: ForgetComponent
+            },
+            {
+                path: "dashboard",
+                canActivate: [AuthGuard],
+                component: DashboardComponent
+            },
+            {
+                path: "orgnization",
+                canActivate: [AuthGuard],
+                canActivateChild: [AuthGuard],
+                children: [
+                    { path: "create", component: OrgnizationCreateComponent },
+                    { path: "edit/:id", component: OrgnizationCreateComponent },
+                    { path: "manage", component: OrgnizationListComponent },
+                ]
+            },
+            {
+                path: "club",
+                canActivate: [AuthGuard],
+                canActivateChild: [AuthGuard],
+                children: [
+                    { path: "create", component: AddEditClubComponent },
+                    { path: "edit/:id", component: AddEditClubComponent },
+                    { path: "manage", component: ClubComponent },
+                ]
+            },
+            {
+                path: "team",
+                canActivate: [AuthGuard],
+                canActivateChild: [AuthGuard],
+                children: [
+                    { path: "create", component: AddEditTeamComponent },
+                    { path: "edit/:id", component: AddEditTeamComponent },
+                    { path: "manage", component: TeamComponent },
+                ]
+            },
+            {
+                path: "player",
+                canActivate: [AuthGuard],
+                canActivateChild: [AuthGuard],
+                children: [
+                    { path: "create", component: AddEditPlayerComponent },
+                    { path: "edit/:id", component: AddEditPlayerComponent },
+                    { path: "manage", component: PlayerComponent },
+                ]
+            }
         ]
     },
-    {
-        path: "club",
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
-        children: [
-            { path: "create", component: AddEditClubComponent },
-            { path: "edit/:id", component: AddEditClubComponent },
-            { path: "manage", component: ClubComponent },
-        ]
-    },
-    {
-        path: "team",
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
-        children: [
-            { path: "create", component: AddEditTeamComponent },
-            { path: "edit/:id", component: AddEditTeamComponent },
-            { path: "manage", component: TeamComponent },
-        ]
-    },
-    {
-        path: "player",
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
-        children: [
-            { path: "create", component: AddEditPlayerComponent },
-            { path: "edit/:id", component: AddEditPlayerComponent },
-            { path: "manage", component: PlayerComponent },
-        ]
-    }
 ];
